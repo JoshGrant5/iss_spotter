866d8330f44d4c1c4740feefa94f446c684a9d10
JoshGrant5 <joshgg@icloud.com>
2020-09-24 23:38:38 +0000

    Deleted prior test conditios

--- index.js
+++ index.js
@@ -7,29 +7,3 @@ nextISSTimesForMyLocation((error, passTimes) => {
   // success, print out the deets!
   console.log(passTimes);
 });
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-// fetchCoordsByIP('207.216.89.139', (error, data) => {
-//   if (error) {
-//     console.log('Could not find coordinates!', error);
-//     return;
-//   } else {
-//     console.log('It worked! Returned coordinates: ', data);
-//   }
-// });
-
-// fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, data) => {
-//   if (error) {
-//     console.log('Could not find FlyOverTime!', error);
-//     return;
-//   } else {
-//     console.log('It worked! Returned FlyOverTime: ', data);
-//   }
-// });
